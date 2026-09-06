6ef0f6849b573401433a6344dccd7560219da282
Arthur Lírio Aquiles <arthuraquiles.work@gmail.com>
2026-09-05 23:51:17 -0300

    fix: set base path for GitHub Pages deployment
    Update the base URL to '/CronoSite/' to ensure assets are correctly resolved when hosted on GitHub Pages.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    // Definimos explicitamente o nome do repositório como base para evitar erro de assets no GitHub Pages
+    base: '/CronoSite/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
